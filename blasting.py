53bc39972caa5081982f3dc9d74f2b348af8b562
hajya <alihaji484@gmail.com>
2012-05-07 14:33:34 -0700

    uh yeah

--- blasting.py
+++ blasting.py
@@ -5,12 +5,13 @@ from Bio.Blast import NCBIXML
 blast_records = NCBIXML.read(result_handle)
 
 E_VAL = 0.0001
-i = 0
 titles = []
 for b in blast_records.alignments:
 	for hsp in b.hsps:
 		if hsp.expect < E_VAL:
 			titles.append(b.title)			
-for i in titles:
-	print(i)
-	print('\n')
+#for i in titles:
+	c = 0
+	for m in titles[0]:
+		c += 1
+		print(m)
