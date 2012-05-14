c75466762a5ebc0d58c7e1d43eeb2264d20838a8
James Edward Bucher <bucherj@CF498-xx.cs.wwu.edu>
2012-05-14 12:51:45 -0700

    make the run-hadoop.sh script easier to read and more compact, also more flexible

--- aarc_scoring/run-hadoop.sh
+++ aarc_scoring/run-hadoop.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-HADOOP_PATH=/home/bucherj/hadoop/bin/hadoop
-HADOOP_STREAMING_PATH=/home/bucherj/hadoop/contrib/streaming/hadoop-*streaming*.jar 
+HADOOP_PATH=~/hadoop/bin/hadoop
+HADOOP_STREAMING_PATH=~/hadoop/contrib/streaming/hadoop-*streaming*.jar 
 
 
-MAPPER_FILE=/home/bucherj/AARC/test1/map.py
-REDUCER_FILE=/home/bucherj/AARC/test1/reduce.py
-AARC_FILE=/home/bucherj/AARC/test1/aarc.py
-SCORING_FILE=/home/bucherj/AARC/test1/scoring.py
+MAPPER_FILE=./map.py
+REDUCER_FILE=./reduce.py
+AARC_FILE=./aarc.py
+SCORING_FILE=./scoring.py
 
 
 INPUT_LOCATION=/user/bucherj/aarc
@@ -16,6 +16,3 @@ OUTPUT_LOCATION=/user/bucherj/aarc-output
 $HADOOP_PATH dfs -rmr $OUTPUT_LOCATION
 
 $HADOOP_PATH jar $HADOOP_STREAMING_PATH -D mapred.max.split.size=10MB -D mapred.maps.tasks=1 -D mapred.reduce.tasks=6 -file $AARC_FILE -file $SCORING_FILE -file $MAPPER_FILE -mapper $MAPPER_FILE -file $REDUCER_FILE -reducer $REDUCER_FILE -input $INPUT_LOCATION -output $OUTPUT_LOCATION 
-
-
-cat test-data-formatted | $MAPPER_FILE | $REDUCER_FILE > /dev/null
