488dd25a329bf33122a97cd980da6e4d273bbf4a
James Edward Bucher <bucherj@CF498-xx.cs.wwu.edu>
2012-05-14 12:46:24 -0700

    re-organize files, move test data to a data subdir

--- test1/gen-formatted-output.py
+++ test1/gen-formatted-output.py
@@ -2,8 +2,8 @@ from aarc import *
 import json
 
 
-A = alignments_to_json('rhoa-aligned-no-predicted.fa', 'fasta')
-B = alignments_to_json('rock-aligned-no-predicted.fa', 'fasta')
+A = alignments_to_json('data/rhoa-aligned-no-predicted.fa', 'fasta')
+B = alignments_to_json('data/rock-aligned-no-predicted.fa', 'fasta')
 
 print json.dumps(A)
 print json.dumps(B)
