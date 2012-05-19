d5cfc70e5b5c66247b414068b5365e01bdf8ade6
Tyler Alan Land <landt@CF498-xx.cs.wwu.edu>
2012-05-19 15:37:07 -0700

    Temp commit by: James Bucher

--- blasting.py
+++ blasting.py
@@ -15,46 +15,46 @@ handler = open("toAlign.fasta",'w')
 prots['proteinID'] = record.description
 prots['SearchQuery'] = str(record.seq)
 for b in blast_records.alignments:
-	for hsp in b.hsps:
-		if hsp.expect < E_VAL:
-			c = 1
-			start = 0;
-			finish = 0
-			for m in b.title:
-				if(m == '['):
-					start = c
-				if(m == ']'):
-					finish= c-1
-					this = b.title[start:finish]
-					
-					if(this not in titles 
-						and this != 'synthetic construct' 
-						and b.title[33:42] != 'PREDICTED'):
-						titles[this] = this
-						unqList['id'] = specCount 
-						unqList['description'] = b.title
-						handler.write('>')
-						handler.write(str(b.title))
-						handler.write('\n')
-						handler.write(str(hsp.sbjct))
-						handler.write('\n\n')
-						
-					#	print(this)O
-					#	print(hsp.sbjct)
-						specCount += 1		
-						thelist.append(unqList)
-						
-						break
-				c += 1
+    for hsp in b.hsps:
+        if hsp.expect < E_VAL:
+            c = 1
+            start = 0;
+            finish = 0
+            for m in b.title:
+                if(m == '['):
+                    start = c
+                if(m == ']'):
+                    finish= c-1
+                    this = b.title[start:finish]
+                    
+                    if(this not in titles 
+                        and this != 'synthetic construct' 
+                        and b.title[33:42] != 'PREDICTED'):
+                        titles[this] = this
+                        unqList['id'] = specCount 
+                        unqList['description'] = b.title
+                        handler.write('>')
+                        handler.write(str(b.title))
+                        handler.write('\n')
+                        handler.write(str(hsp.sbjct))
+                        handler.write('\n\n')
+                        
+                    #   print(this)O
+                    #   print(hsp.sbjct)
+                        specCount += 1      
+                        thelist.append(unqList)
+                        
+                        break
+                c += 1
 prots['Proteins'] = thelist
 theMAINlist.append(prots)
 thelist = []
 
 
 '''for m in theMAINlist:
-	print(m['proteinID'])
-	print(m['SearchQuery'])
-	for prots in m['Proteins']:
-		print(prots['id'])
-		print(prots['description'])
-		print(prots['seq'])'''
+    print(m['proteinID'])
+    print(m['SearchQuery'])
+    for prots in m['Proteins']:
+        print(prots['id'])
+        print(prots['description'])
+        print(prots['seq'])'''
--- getmore.py
+++ getmore.py
@@ -3,89 +3,131 @@ from Bio import SeqIO
 from Bio.Blast import NCBIXML
 from Bio.Align.Applications import ClustalwCommandline
 from Bio import AlignIO
+import re
+import sys
 
-
-E_VAL = 0.0001
+E_VAL_THRESHOLD = 0.0001
 titles = {}
 theMAINlist = []
 prots = {}
 outfile = 'out.phy'
 formata = 'phylip'
 
+def make_uid(x):
+    def inc():
+        return x + 1
+    return inc
+
+UID = make_uid(-1)
+
 def makeXML(rec):
-	#call to blast
-	print(rec)
-	result_handle = NCBIWWW.qblast("blastp", "nr", rec.format("fasta"))
-	#put it into an xml file
-	save_file = open("my_blast.xml", "w")
-	save_file.write(result_handle.read())
-	save_file.close()
-	result_handle.close()
+    #call to blast
+    print(rec)
+    result_handle = NCBIWWW.qblast("blastp", "nr", rec.format("fasta"))
+    #put it into an xml file
+    save_file = open("my_blast.xml", "w")
+    save_file.write(result_handle.read())
+    save_file.close()
+    result_handle.close()
 
 
 def parseXML():
-	result_handle = open("my_blast.xml")
-	b = NCBIXML.read(result_handle)
-	return b
+    result_handle = open("my_blast.xml")
+    b = NCBIXML.read(result_handle)  #read() is meant for a single query.  Should use parse()
+    return b
 
-def getHomoLs(bb):
-	handler = open("toAlign.fasta",'w')
-	unqList = {}
-	specCount = 1
-	for b in bb.alignments:
-		for hsp in b.hsps:
-			if hsp.expect < E_VAL:
-				c = 1
-				start = 0
-				finish = 0
-				count = 0
-				endTit = 0
-				for m in b.title:
-					if(m == '|'):
-						if(count == 1):
-							endTit = c-1
-							count = 2
+def getHomologues(blast_records, searchQuery):
+    '''Takes in a blast NCBIXML read and turns it into a
+    homologue, returns a dictionary containing the following structure:
+    {'searchQuery': <the function argument searchQuery>,
+     'protiens':{ 'species-1': [{'id':'species-1', 'seq':<protien-seq-1>, 'desc':<blast-desc-1>, 'uid': 1},
+                                {'id':'species-1', 'seq':<protien-seq-2>, 'desc':<blast-desc-2>, 'uid': 2},
+                               ],
+                  'species-2': [{'id':'species-2', 'seq':<protien-seq-3>, 'desc':<blast-desc-3>, 'uid': 3}
+                               ],
+                  . . .
+                }
+    }'''
 
-						if(count == 0):
-							count = 1
-				
-					if(m == '['):
-						start = c
-					if(m == ']'):
-						finish= c-1
-						this = b.title[start:finish]
-						ida = b.title[0:endTit]
-						if(this not in titles 
-							and this != 'synthetic construct' 
-							and b.title[33:42] != 'PREDICTED'):
-							titles[this] = this
-							unqList[b.title[0:10]] = this
-							unqList[b.title[0:10]+'COUNT'] = specCount 
-							unqList[b.title[0:10]+'descr'] = b.title
-							handler.write('>')
-							handler.write(str(b.title))
-							handler.write('\n')
-							handler.write(str(hsp.sbjct))
-							handler.write('\n\n')
-						#	print(this)O
-						#	print(hsp.sbjct)
-							specCount += 1		
-							break
-					c += 1
-	return unqList
-def align():
-	
-	cline = ClustalwCommandline("clustalw", infile='toAlign.fasta',outfile=outfile,align='true',output='PHYLIP')
-	cline()
-	alignment = AlignIO.parse(outfile,formata).next()
-	return alignment
+    global E_VAL_THRESHOLD
+    homologue = {}
+    homologue['searchQuery'] = searchQuery
+    homologue['protiens'] = {}
+    protiens = homologue['protiens']
+    for alignment in blast_records.alignments:
+        if len(alignment.hsps) != 1: #ERROR CONDITION
+            print >> sys.stderr, "ERROR: unexpected blast results" 
+            continue
+        if alignment.hsps[0].expect > E_VAL_THRESHOLD:
+            continue
+        species = re.findall("\[.*?\]", alignment.title)
+        species = set(species) # get only unique elements
+        if len(species) == 1:
+            species_name = list(species)[0]
+            if species_name not in protiens:
+                protiens[species_name] = []
+            species_protien_list = protiens[species_name]
+            temp = {}
+            temp['uid'] = "FOO" #UID() 
+            temp['id'] = list(species)[0]
+            temp['description'] = "TEMP"#alignment.title
+            temp['score'] = alignment.hsps[0].score
+            temp['seq'] = "TEMP" #alignment.hsps[0].sbjct
+            species_protien_list.append(temp)
+    return homologue
+
+def align(homologue):
+    '''Takes in a homologue from getHomologues() and
+    aligns all of the sequences that it contains'''
+    cline = ClustalwCommandline("clustalw", infile='toAlign.fasta',outfile=outfile,align='true',output='PHYLIP')
+    cline()
+    alignment = AlignIO.parse(outfile,formata).next()
+    return alignment
+
 def makeRec():
-	thelist =[]
+    thelist =[]
+
+def alignments_to_json(filename, fileFormat):          #Experimental.  filename="out.phy", format="phylip"
+    '''Opens a file with one alignment and converts it to a list that can
+    be exported to a json string'''
+    alignments = AlignIO.read(open(filename), fileFormat)
+    homologue = {}
+    homologue["protienID"] = alignments[0].description
+    homologue["searchQuery"] = str(alignments[0].seq)
+    protiens = []
+    for element in alignments:
+        temp = {}
+        temp['id'] = element.id
+        temp['description'] = element.description
+        temp['seq'] = str(elemenet.seq)
+        protiens.append(temp)
+    homologue["protiens"] = protiens
+    return homologue
 
 if __name__ == "__main__":
-	#format the seq from the file
-	record = SeqIO.read(open("hemo.fasta"), format="fasta")
-	#makeXML(record)
-	blast_records = parseXML()
-	unqList = getHomoLs(blast_records)
-	a = align(unqList)
+    #format the seq from the file
+    record = SeqIO.read(open("hemo.fasta"), format="fasta")
+    #makeXML(record)
+    blast_records = parseXML()
+    unqList = getHomoLs(blast_records)
+    print("-------------------------")
+    print(unqList)
+    print("-------------------------")
+    #a = align(unqList)
+    a = align()
+
+    '''Desired Output:  A json string of the following format...
+    A list of homologues.  Homologues are dictionaries
+
+    [ { SearchQuery: " ... "                                             #Homologue number 1
+        protiens: [ { "id":   unique-species-id
+                      "seq":  aligned-sequence-of-uniform-length
+                      "desc": " ... "
+                    },
+                    additional dictionaries for each unique-species-id...
+                  ]
+      },
+      { ... }, #Homologue number 2...
+      ...
+    ]
+    '''
